0b4a256154e6c951f354e1bd885622cd33390f98
Elitesun <elitesun.dev@gmail.com>
2025-03-15 09:02:19 +0000

    fixing some little bugs

--- la-a-la/components/securityInfo/PasswordInfo.tsx
+++ la-a-la/components/securityInfo/PasswordInfo.tsx
@@ -15,7 +15,7 @@ confirmPassword: string;
 // ---------------------------------------- Component Definition ---------------------------------------- //
 
 const PasswordInfo = ({ password = "alexSunday123",
-confirmPassword= "alzxSunday123",
+confirmPassword= "",
 }: Pass) =>{
 const [showPassword , setShowPassword] = useState(false)
 const handleShowPassword = () => {
@@ -47,10 +47,6 @@ const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     };
 
     const handleCancel = () => {
-    setFormData({
-    password,
-    confirmPassword
-    });
     setIsEditing(false);
     };
 
@@ -86,10 +82,20 @@ const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         <div className="relative">
             {isEditing ? (
             <>
-                <input name="password" value={formData.password} onChange={handleInputChange} {...(showPassword ? {
-                    type: "text" } : { type: "password" })}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+            <h4>New Password</h4>
+                <div className="relative">
+                    <input name="password" value={formData.password} onChange={handleInputChange} {...(showPassword ? {
+                        type: "text" } : { type: "password" })} 
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+                    <button type="button" onClick={handleShowPassword}
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none">
+                        {showPassword ?
+                        <FaEye className="w-5 h-5" /> :
+                        <FaRegEyeSlash className="w-5 h-5" />}
+                    </button>
+                </div>
                 <div className="relative mt-4">
+                    <h2>Confirm Password</h2>
                     <input name="confirmPassword" value={formData.confirmPassword} onChange={handleInputChange}
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" {...(showConfirmPassword ?
                     { type: "text" } : { type: "password" })} />
@@ -100,12 +106,6 @@ const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
                         <FaRegEyeSlash className="w-5 h-5" />}
                     </button>
                 </div>
-                <button type="button" onClick={handleShowPassword}
-                    className="absolute right-3 top-[22px] transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none">
-                    {showPassword ?
-                    <FaEye className="w-5 h-5" /> :
-                    <FaRegEyeSlash className="w-5 h-5" />}
-                </button>
                 <PasswordValidation 
                     password={formData.password}
                     confirmPassword={formData.confirmPassword}
