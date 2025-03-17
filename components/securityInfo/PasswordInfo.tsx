c16d66c93a799f576e60ea4c3429f2d66b67b138
Elitesun <elitesun.dev@gmail.com>
2025-03-17 20:00:44 +0000

    renamed and reorganized files

--- components/securityInfo/PasswordInfo.tsx
+++ components/securityInfo/PasswordInfo.tsx
@@ -102,15 +102,17 @@ const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
                 </div>
                 <div className="relative mt-4">
                     <h2>Confirm Password</h2>
-                    <input name="confirmPassword" value={formData.confirmPassword} onChange={handleInputChange}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" {...(showConfirmPassword ?
-                    { type: "text" } : { type: "password" })} />
-                    <button type="button" onClick={handleShowConfirmPassword}
-                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none">
-                        {showConfirmPassword ?
-                        <FaEye className="w-5 h-5" /> :
-                        <FaRegEyeSlash className="w-5 h-5" />}
-                    </button>
+                    <div className="relative">
+                        <input name="confirmPassword" value={formData.confirmPassword} onChange={handleInputChange}
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent pr-10" {...(showConfirmPassword ?
+                        { type: "text" } : { type: "password" })} />
+                        <button type="button" onClick={handleShowConfirmPassword}
+                            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none">
+                            {showConfirmPassword ?
+                            <FaEye className="w-5 h-5" /> :
+                            <FaRegEyeSlash className="w-5 h-5" />}
+                        </button>
+                    </div>
                 </div>
                 <PasswordValidation 
                     password={formData.password}
